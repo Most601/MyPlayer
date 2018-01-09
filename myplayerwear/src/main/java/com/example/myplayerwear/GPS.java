5fdd30364ebda158d5714732eec6122b07ff00f5
Most601 <most601@gmail.com>
2018-01-09 09:37:55 +0200

    GPS Wear

--- myplayerwear/src/main/java/com/example/myplayerwear/GPS.java
+++ myplayerwear/src/main/java/com/example/myplayerwear/GPS.java
@@ -1,61 +1,93 @@
 package com.example.myplayerwear;
 
 
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
+import android.support.annotation.NonNull;
+import android.support.wearable.activity.WearableActivity;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationListener;
+
+
+import java.util.concurrent.TimeUnit;
 
 
 /**
  * Created by Most601 on 15/12/2017.
  */
 
-public class GPS implements LocationListener {
+public class GPS extends WearableActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private LocationManager locationManager;
     private String latitude;
     private String longitude;
     private Criteria criteria;
     private String provider;
+    private Context context ;
+    private GoogleApiClient googleApiClient;
+
+
+
 
 
     public GPS(Context context) {
+        this.context = context;
+        onStart();
+    }
 
 
 
-        locationManager = (LocationManager) context
-                .getSystemService(Context.LOCATION_SERVICE);
-        criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);
-        provider = locationManager.getBestProvider(criteria, true);
-
-
-        if (ActivityCompat.checkSelfPermission(context , Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
-                (context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return ;
+    // Connect to Google Play Services when the Activity starts
+    @Override
+    protected void onStart() {
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(context)
+                    .addApi(LocationServices.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
         }
-        locationManager.requestLocationUpdates("gps", 5000,0, this);
-        setMostRecentLocation(locationManager.getLastKnownLocation(provider));
+        googleApiClient.connect();
+    }
+
+
+
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+
+        // Create the LocationRequest object
+        LocationRequest locationRequest = LocationRequest.create();
+        // Use high accuracy
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        // Set the update interval to 2 seconds
+        locationRequest.setInterval(TimeUnit.SECONDS.toMillis(2));
+        // Set the fastest update interval to 2 seconds
+        locationRequest.setFastestInterval(TimeUnit.SECONDS.toMillis(2));
+        // Set the minimum displacement
+        locationRequest.setSmallestDisplacement(2);
+
+        // Register listener using the LocationRequest object
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
 
     }
 
-    private void setMostRecentLocation(Location lastKnownLocation) {
-
-    }
 
     public String getLatitude() {
         return latitude;
@@ -66,8 +98,6 @@ public class GPS implements LocationListener {
     }
 
 
-
-
     @Override
     public void onLocationChanged(Location location) {
         double lon = (double) (location.getLongitude());/// * 1E6);
@@ -76,18 +106,20 @@ public class GPS implements LocationListener {
         longitude = lon + "";
     }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
 
+    @Override
+    protected void onStop() {
+
+        if (googleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+            googleApiClient.disconnect();
+        }
+        super.onStop();
     }
 
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
 
     @Override
-    public void onProviderDisabled(String provider) {
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
--- myplayerwear/src/main/java/com/example/myplayerwear/MainActivity.java
+++ myplayerwear/src/main/java/com/example/myplayerwear/MainActivity.java
@@ -1,40 +1,151 @@
+//package com.example.myplayerwear;
+//
+//import android.os.Bundle;
+//import android.support.wearable.activity.WearableActivity;
+//import android.view.View;
+//import android.widget.TextView;
+//
+//public class MainActivity extends WearableActivity {
+//
+//    private TextView mTextViewLad;
+//    private TextView mTextViewLed;
+//    GPS gps ;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        mTextViewLad = (TextView) findViewById(R.id.text);
+//        mTextViewLed = (TextView) findViewById(R.id.text);
+//
+//        gps = new GPS(this);
+//        // Enables Always-on
+//        setAmbientEnabled();
+//    }
+//
+//    public void Gps_data(View view) {
+//
+//        mTextViewLad.setText(gps.getLatitude());
+//        mTextViewLed.setText(gps.getLongitude());
+//
+//
+//
+//
+//
+//    }
+//
+//    public void blbl4blblb(View view) {
+//    }
+//}
+
+
 package com.example.myplayerwear;
 
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MainActivity extends WearableActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
 
-    private TextView mTextViewLad;
-    private TextView mTextViewLed;
-    GPS gps ;
+    public class MainActivity extends WearableActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener  {
+
+    public static final String WEARABLE_MAIN = "WEARABLE_MAIN";//logcat debugging.
+    private Node mNode;//represents the phone that I want to communicate with from the watch.
+    private GoogleApiClient mGoogleApiClient;//to connect and send messages.
+    private static final String WEAR_PATH = "/from-wear";//to synchronize messages between the devices.
+
+
+    private TextView latitude;
+    private TextView longitude;
+    GPS gps;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        latitude = (TextView) findViewById(R.id.lad);
+        longitude = (TextView) findViewById(R.id.led);
+        gps = new GPS(this );
+        //Initialize mGoogleApiClient.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this) //Callbacks from node - success or fails.
+                .addOnConnectionFailedListener(this) //If I had a fail connection.
+                .build();//create my API object.
 
-        mTextViewLad = (TextView) findViewById(R.id.text);
-        mTextViewLed = (TextView) findViewById(R.id.text);
+    }
 
-        gps = new GPS(this);
-        // Enables Always-on
-        setAmbientEnabled();
+
+    @Override
+    //From addConnectionCallbacks.
+    public void onConnected(@Nullable Bundle bundle) {
+        //Which node to connect to.
+        Wearable.NodeApi.getConnectedNodes(mGoogleApiClient)
+                .setResultCallback(new ResultCallback<NodeApi.GetConnectedNodesResult>() {
+                    @Override
+                    public void onResult(@NonNull NodeApi.GetConnectedNodesResult nodes) {
+                        //Find the node I want to communicate with.
+                        for (Node node : nodes.getNodes()) {
+                            if (node != null && node.isNearby()) {
+                                mNode = node;
+                                Log.d(WEARABLE_MAIN, "?????????????????????????????????????????Connected to " + mNode.getDisplayName());
+                            }
+                        }
+                        if (mNode == null) {
+                            Log.d(WEARABLE_MAIN, "Not connected!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                        }
+
+                    }
+                }) //returns a set of nods.
+        ;
+
+    }
+
+    @Override
+    //From addConnectionCallbacks.
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    //From addOnConnectionFailedListener.
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    //Connect to the external node.
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    //Clean up the connection as the activity stops.
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
     }
 
     public void Gps_data(View view) {
 
-        mTextViewLad.setText(gps.getLatitude());
-        mTextViewLed.setText(gps.getLongitude());
+        latitude.setText(gps.getLongitude());
+        longitude.setText(gps.getLatitude());
 
-
-
-
-
-    }
-
-    public void blbl4blblb(View view) {
     }
 }

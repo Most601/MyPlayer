bdc296d9a87d43c509a434db2056290198e7cd8e
Most601 <most601@gmail.com>
2018-04-10 16:24:19 +0300

    Orientation; sensor add

---
+++ myplayerwear/src/main/java/com/example/myplayerwear/Orientation.java
@@ -0,0 +1,72 @@
+package com.example.myplayerwear;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+import static android.content.Context.SENSOR_SERVICE;
+
+/**
+ * Created by Most601 on 04/04/2018.
+ */
+
+public class Orientation implements SensorEventListener {
+
+    private static final String TAG = "Orientation";
+
+    private Sensor mySensor;
+    private SensorManager SM;
+    private SendToPhone STP ;
+
+    public Orientation(Context context) {
+        SM = (SensorManager) context.getSystemService(SENSOR_SERVICE);
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+        STP = SendToPhone.getInstance(context);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        //print on datashow
+        try {
+            DataShow.print("Orientation",event);
+        }catch (Exception e){
+        }
+
+        STP.sendSensorData(
+                event.sensor.getStringType() ,
+                event.sensor.getType(),
+                event.accuracy,
+                event.timestamp,
+                event.values);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public void startMeasurement(){
+        // Register sensor Listener
+        if (SM != null) {
+            if (mySensor != null) {
+                SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+            } else {
+                Log.w(TAG, "No Orientation found");
+            }
+        }
+    }
+
+    public void stopMeasurement() {
+        // unRegister sensor Listener
+        if (SM != null) {
+            SM.unregisterListener(this);
+        }
+    }
+
+
+
+
+}
